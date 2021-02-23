ae8668ee3d8b2b9e734621d4292ef95c0f25f97f
Valerie Kolling <valerie.kolling@gmx.de>
2021-02-23 13:39:44 +0100

    Obstacles Controller Actions and Views for index, create, update

--- app/controllers/obstacles_controller.rb
+++ app/controllers/obstacles_controller.rb
@@ -1,14 +1,24 @@
 class ObstaclesController < ApplicationController
   def index
+    @obstacles = Obstacle.all
   end
 
   def new
+    @obstacle = Obstacle.new
   end
 
   def update
   end
 
   def create
+    @obstacle = Obstacle.new(obstacle_params)
+    @user = current_user
+    @obstacle.user = @user
+    if @obstacle.save
+      redirect_to obstacle_show_path(@obstacle)
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -18,5 +28,11 @@ class ObstaclesController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+
+  def obstacle_params
+    params.require(:obstacle).permit(:name, :category, :price, :availability, :location)
   end
 end
