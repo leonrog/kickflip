4d0d64e24e580e4b6df1bbcf83c1e327fbd1b4b7
Valerie Kolling <valerie.kolling@gmx.de>
2021-02-23 17:47:22 +0100

    Bookings create action working, other actions still Work in progress

--- app/models/obstacle.rb
+++ app/models/obstacle.rb
@@ -1,6 +1,6 @@
 class Obstacle < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :name, :category, :price, :availability, :location, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates :category, inclusion: { in: %w(flatrail curb kicker quarter ramp handrail other) }
