62d8beab7336d4f593faeb85616c90adaf21af25
Laurenz Gerold <skatezar@gmail.com>
2021-02-25 17:44:44 +0100

    search bar feature added, styling not done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-    @obstacles = Obstacle.all
+    if params[:query].present?
+      @obstacles = policy_scope(Obstacle).where("name ILIKE ?", "%#{params[:query]}%")
+    else
+      @obstacles = policy_scope(Obstacle).order(created_at: :desc)
+    end
   end
 end
