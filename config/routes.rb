243c6b5811b43fc3d2d1c84ab26c4e98f3ed0d6d
Valerie Kolling <valerie.kolling@gmx.de>
2021-02-25 18:03:45 +0100

    Booking pundit WIP

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   put '/myprofile', to: 'users#update', as: :update_my_profile
   root to: 'pages#home'
   resources :obstacles do
-    resources :bookings, only:[:index, :new, :create, :delete, :show]
+    resources :bookings, only:[:index, :new, :create, :show]
   end
+  resources :bookings, only: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
